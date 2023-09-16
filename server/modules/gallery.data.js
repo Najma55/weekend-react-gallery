0cd0344ccac79ccdf5db6dd9d465d9a9f7aa264a
Najma Jama <Najma55.nj@gmail.com>
2023-09-16 13:53:01 -0500

    added gallery data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/bear.jpeg', description: 'Photo of a bear taken at the Zoo.', likes: 0 },
+    { id: 3, path: 'images/cat.jpeg', description: 'Photo of a cat taken in the backyard.', likes: 0 },
+    { id: 4, path: 'images/lion.jpeg', description: 'Photo of a lion taken at a National Zoo .', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
